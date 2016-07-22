9b6bfd9fe1fbe8a40f587bd474212fc82a51f850
ww <yanruoxinb@github.com>
2016-07-22 18:36:33 +0800

    hello

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,10 +1,11 @@
 'use strict';
-
+console.log("hello");
 function collect_all_even(collection) {
     var array = new Array();
     for(var i=0;i<collection.length;i++)
     {
         if(collection[i]%2 == 0)
+
             array.push(collection[i]);
     }
     return array;
